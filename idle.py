57265cd41c29bec01cd8dc608bf11a75f5fab284
Desarrollos PICSL <desarrollo@pic-sl.com>
2020-05-15 20:07:09 +0100

    Added doc reference

--- idle.py
+++ idle.py
@@ -6,7 +6,7 @@ Returns:
     [type] -- [description]
 """
 
-
+import logging
 import sys
 import time
 
@@ -15,15 +15,34 @@ if sys.platform.startswith('win32'):
 
 if sys.platform.startswith('win32'):
 
+    # Detecting idle time using python
+    # https://stackoverflow.com/questions/911856/detecting-idle-time-using-python
+    #
+    # https://stackoverflow.com/questions/217157/how-can-i-determine-the-display-idle-time-from-python-in-windows-linux-and-mac?noredirect=1&lq=1
+    # TODO: 
+
     class LASTINPUTINFO(Structure):
+        """[summary]
+
+        Arguments:
+            Structure {[type]} -- [description]
+        """
+
         _fields_ = [
             ('cbSize', c_uint),
             ('dwTime', c_int),
         ]
 
     def get_idle_duration():
+        """[summary]
+
+        Returns:
+            [type] -- [description]
+        """
+
         lastInputInfo = LASTINPUTINFO()
         lastInputInfo.cbSize = sizeof(lastInputInfo)
+
         if windll.user32.GetLastInputInfo(byref(lastInputInfo)):
             millis = windll.kernel32.GetTickCount() - lastInputInfo.dwTime
             return millis / 1000.0
@@ -54,7 +73,7 @@ if __name__ == '__main__':
 
     while True:
         duration = get_idle_duration()
-        print(('User idle for %.2f seconds.' % duration))
+        logging.debug(('User idle for %.2f seconds.' % duration))
         time.sleep(1)
 
 # EOF
